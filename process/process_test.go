2727a3027f1fdbc330402e0ca8be2151ff38f3c2
sevennt <sevennt.leslie@gmail.com>
2024-04-15 18:25:43 +0800

    improve test

--- process/process_test.go
+++ process/process_test.go
@@ -6,15 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var Xml = "https://raw.githubusercontent.com/shimo-open/push-seo/v0.0.2/config/example.yml"
-
 func TestGetLocsFromXml(t *testing.T) {
-	// 调用被测试的函数
-	urlSet, err := FetchXML(Xml)
-	assert.NoError(t, err)
-	t.Log("urlSet info", urlSet)
+	cases := []struct {
+		xmlUrl   string
+		expected []string
+	}{
+		{
+			xmlUrl:   "https://raw.githubusercontent.com/shimo-open/push-seo/master/process/example.xml",
+			expected: []string{"https://a.com/path-a/", "https://a.com/path-a/apis"},
+		},
+	}
 
-	locs := ExtractLocsFromSitemap(urlSet)
-	expected := []string{"https://a.com/path-a/", "https://a.com/path-a/apis"}
-	assert.Equal(t, expected, locs)
+	for _, c := range cases {
+		// 调用被测试的函数
+		urlSet, err := FetchXML(c.xmlUrl)
+		assert.NoError(t, err)
+		t.Log("urlSet info", urlSet)
+
+		locs := ExtractLocsFromSitemap(urlSet)
+		assert.Equal(t, c.expected, locs)
+	}
 }
